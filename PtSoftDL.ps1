881fa8788d32ca8d188d778a134f24cd3c16d601
Charlotte.Hong <hunandy14@gmail.com>
2023-01-16 23:10:35 +0900

    完成下載介面

--- PtSoftDL.ps1
+++ PtSoftDL.ps1
@@ -10,6 +10,7 @@ function WebDL {
         [String] $Name,
         [Switch] $OpenDir
     )
+    # 參數設定
     $Dsk = $([Environment]::GetFolderPath('Desktop'))
     $Url -match "[^/]+(?!.*/)" |Out-Null # 獲取連結中的檔名
     $UrlFileName = $Matches[0]
@@ -22,13 +23,44 @@ function WebDL {
         } else { $Path = $Dsk }
     }
     if (!(Test-Path $Path)) {
-        New-Item $Path -ItemType:Directory -Force | Out-Null
+        New-Item $Path -ItemType:Directory -Force |Out-Null
     }
     # 目標檔案完整路徑
     $DstPath  = "$Path\$Name"
     # 下載
-    Start-BitsTransfer $Url $DstPath
+    Start-BitsTransfer $Url $Path
     # 打開資料夾
     if ($OpenDir) { explorer.exe $DstPath }
     return $DstPath
-} # WebDL "https://download.geniusite.com/DG5461441_x64.zip" -TempPath
+} # WebDL "https://github.com/hunandy14/PtSoftDL/raw/master/soft/DG5461441_x64.zip"
+
+
+
+# 下載攜帶版軟體
+function PtSoftDL {
+    param (
+        [Parameter(Position = 0, ParameterSetName = "Down", Mandatory)]
+        [string] $Name,
+        [Parameter(ParameterSetName = "")]
+        [String] $Path,
+        [Switch] $TempPath,
+        [Parameter(ParameterSetName = "")]
+        [string] $ListPath,
+        [switch] $OpenDir,
+        [Parameter(ParameterSetName = "Info")]
+        [switch] $Info
+    )
+    # 獲取清單
+    $ListPath = "raw.githubusercontent.com/hunandy14/PtSoftDL/master/SoftList.json"
+    $Json = Invoke-RestMethod $ListPath
+    # 下載
+    if ($Name) {
+        $Node = $Json.Default.$Name
+        $Url = $Node.Url 
+        WebDL $Url -Path:$Path -TempPath:$TempPath -OpenDir:$OpenDir
+    } elseif ($Info) {
+        $Json.Default.PSObject.Properties.Value
+    }
+}
+# PtSoftDL 'DiskGenius'
+# PtSoftDL -Info
